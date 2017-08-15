2be0a504bcfae1ebccf6eebabc829e24303211ee
DESA KUMAARAN <desakumaaran3@gmail.com>
2017-08-15 13:05:24 +0530

    server socket php

--- server/php/buttonclick.php
+++ server/php/buttonclick.php
@@ -1,16 +1,16 @@
 <?php
-$ip = '192.168.1.5'
-$port = '8723';
+//$ip = '192.168.1.5'
+//$port = 8723;
 
 $led = $_POST['led'];
 $action = $_POST['action'];
 
-$msg = $led +'~'+$action;
+$msg = $led.'~'.$action;
 
-$client = stream_socket_client($ip . ":" . $port, $errno, $errorMessage);
+$client = stream_socket_client("tcp://192.168.1.5:8723", $errno, $errorMessage);
 if ($client === false) {
 //$_SESSION["msg"] .= "<br/>Cannot connect to Judge: Contact Admin";
 }
 fwrite($client, $msg);
 fclose($client);
->
+?>
